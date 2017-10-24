719a043eaade272128c577d195b3e126e5b4e3e6
Sören Henning <post@soeren-henning.de>
2017-10-24 18:47:24 +0200

    added importer for other graph files, improved tester's output

--- src/main/java/DimacsImporter.kt
+++ src/main/java/DimacsImporter.kt
@@ -2,52 +2,70 @@ import com.google.common.graph.GraphBuilder
 import com.google.common.graph.MutableGraph
 import java.io.Reader
 import java.io.BufferedReader
-import java.io.StringReader
 import java.util.HashMap
 import java.util.Arrays
 
 object DimacsImporter {
 
-    fun importGraph(dimacsGraph: String) : MutableGraph<Int> {
-        val graphBuilder = GraphBuilder.undirected()
-        return importGraph(StringReader(dimacsGraph), { n -> n} , graphBuilder)
+    fun importGraph(
+            reader: Reader,
+            builder: GraphBuilder<Any?> = GraphBuilder.undirected(),
+            nodeIds: Boolean = false
+    ): MutableGraph<Int> {
+        return importGraph(reader, builder, nodeIds, {n -> n})
     }
 
-    fun importGraph(reader: Reader) : MutableGraph<Int> {
-        val graphBuilder = GraphBuilder.undirected()
-        return importGraph(reader, { n -> n} , graphBuilder)
-    }
-
-    fun <V> importGraph(reader: Reader, vertexBuilder: (Int) -> V, builder: GraphBuilder<Any?>): MutableGraph<V> {
+    fun <V> importGraph(
+            reader: Reader,
+            builder: GraphBuilder<Any?> = GraphBuilder.undirected(),
+            nodeIds: Boolean = false,
+            vertexBuilder: (Int) -> V
+    ): MutableGraph<V> {
         val graph = builder.build<V>()
 
         val bufferedReader = reader as? BufferedReader ?: BufferedReader(reader)
 
-        // add nodes
+        // create nodes map
         val size = readNodeCount(bufferedReader)
         val vertices = HashMap<Int, V>(size)
-        for (id in 1..size) {
-            val vertex = vertexBuilder.invoke(id)
-            vertices.put(id, vertex)
-            graph.addNode(vertex)
+
+        var readLineResult = readNextLine(bufferedReader)
+
+        // add nodes
+        if (nodeIds) {
+            while (readLineResult is ReadLineResult.Column && readLineResult[0] == "n") {
+                val vertexId = try {
+                    Integer.parseInt(readLineResult[1])
+                } catch (e: NumberFormatException) {
+                    throw IllegalArgumentException("Failed to parse node:" + e.message, e)
+                }
+                val vertex = vertexBuilder.invoke(vertexId)
+                vertices.put(vertexId, vertex)
+                graph.addNode(vertex)
+                readLineResult = readNextLine(bufferedReader)
+            }
+        } else {
+            for (id in 1..size) {
+                val vertex = vertexBuilder.invoke(id)
+                vertices.put(id, vertex)
+                graph.addNode(vertex)
+            }
         }
 
         // add edges
-        var readLineResult = readNextLine(bufferedReader)
         while (readLineResult is ReadLineResult.Column) {
-            val cols = readLineResult.content
-            if (cols[0] == "e" || cols[0] == "a") {
-                if (cols.size < 3) {
-                    throw IllegalArgumentException("Failed to parse edge:" + Arrays.toString(cols))
+            if (readLineResult[0] == "e" || readLineResult[0] == "a") {
+                if (readLineResult.size < 3) {
+                    throw IllegalArgumentException("Failed to parse edge:" + readLineResult)
                 }
                 val source = try {
-                    Integer.parseInt(cols[1])
+                    Integer.parseInt(readLineResult[1])
                 } catch (e: NumberFormatException) {
                     throw IllegalArgumentException("Failed to parse edge source node:" + e.message, e)
                 }
 
                 val target = try {
-                    Integer.parseInt(cols[2])
+                    Integer.parseInt(readLineResult[2])
                 } catch (e: NumberFormatException) {
                     throw IllegalArgumentException("Failed to parse edge target node:" + e.message, e)
                 }
@@ -76,30 +94,43 @@ object DimacsImporter {
 
     private fun readNodeCount(reader: BufferedReader): Int {
         val readLineResult = readNextLine(reader)
-        val cols = when(readLineResult) {
+        when (readLineResult) {
             is ReadLineResult.EndOfStream -> throw IllegalArgumentException("Failed to read number of vertices.")
-            is ReadLineResult.Column -> readLineResult.content
-        }
-        if (cols[0] == "p") {
-            if (cols.size < 3) {
-                throw IllegalArgumentException("Failed to read number of vertices.")
-            }
-            val nodes = try {
-                Integer.parseInt(cols[2])
-            } catch (e: NumberFormatException) {
-                throw IllegalArgumentException("Failed to read number of vertices.")
-            }
+            is ReadLineResult.Column ->  {
+                if (readLineResult[0] == "p") {
+                    if (readLineResult.size < 3) {
+                        throw IllegalArgumentException("Failed to read number of vertices.")
+                    }
+                    val nodes = try {
+                        Integer.parseInt(readLineResult[2])
+                    } catch (e: NumberFormatException) {
+                        throw IllegalArgumentException("Failed to read number of vertices.")
+                    }
 
-            if (nodes < 0) {
-                throw IllegalArgumentException("Negative number of vertices.")
+                    if (nodes < 0) {
+                        throw IllegalArgumentException("Negative number of vertices.")
+                    }
+                    return nodes
+                }
+                throw IllegalArgumentException("Failed to read number of vertices.")
             }
-            return nodes
         }
-        throw IllegalArgumentException("Failed to read number of vertices.")
     }
 
     private sealed class ReadLineResult() {
         data class Column(val content: Array<String>): ReadLineResult() {
+
+            val size
+                get() = content.size
+
+            operator fun get(index: Int): String {
+                return content[index]
+            }
+
+            override fun toString(): String {
+                return Arrays.toString(content)
+            }
+
             override fun equals(other: Any?): Boolean {
                 if (this === other) return true
                 if (javaClass != other?.javaClass) return false
@@ -119,41 +150,4 @@ object DimacsImporter {
         object EndOfStream: ReadLineResult()
     }
 
-}
-
-fun main(args: Array<String>) {
-
-    val s = """p edge 8 18
- n 0
- n 1
-n 2
-n 3
-n 4
-n 5
-n 6
-n 7
-e 4 1
-e 4 2
-e 5 1
-e 5 3
-e 5 4
-e 6 1
-e 6 2
-e 6 3
-e 7 1
-e 7 2
-e 7 3
-e 7 4
-e 8 1
-e 8 2
-e 8 3
-e 8 4
-e 8 5
-e 8 7
-"""
-    val t = "p edge 2 1\nn 1\nn 2\ne 1 2";
-
-    val graph = DimacsImporter.importGraph(s)
-
-    println(graph)
 }
--- src/main/java/Tester.kt
+++ src/main/java/Tester.kt
@@ -11,9 +11,7 @@ fun main(args: Array<String>) {
 
     val dimacsImporter = DimacsImporter
 
-    //eil51.tsp.dgf
-    //celar06-wpp.dgf
-    val graphFiles = listOf(
+    val dimacsGraphFiles = listOf(
             "1awd.dgf",
             "BN_28.dgf",
             "miles1500.dgf",
@@ -21,26 +19,38 @@ fun main(args: Array<String>) {
             "queen8_12.col",
             "zeroin.i.1.col"
     )
+    val corruptedDimacsGraphFiles = listOf(
+            "eil51.tsp.dgf",
+            "celar06-wpp.dgf"
+    )
 
-    val graphs = graphFiles.asSequence().map{ Pair(it, dimacsImporter.importGraph(getClasspathFileReader("example-graphs/" + it))) }
-    val iterations = listOf(10, 100)
+    val dimacsGraphs = dimacsGraphFiles.asSequence().map{ Pair(it, DimacsImporter.importGraph(getClasspathFileReader("example-graphs/" + it))) }
+    val corruptedDimacsGraphs = corruptedDimacsGraphFiles.asSequence().map{ Pair(it, DimacsImporter.importGraph(getClasspathFileReader("example-graphs/" + it),nodeIds = true)) }
+    val graphs = dimacsGraphs + corruptedDimacsGraphs
+    val iterations = listOf(10, 50, 100)
     val firstTieBreakers = mapOf<String, (Graph<Int>) -> (Collection<Int>) -> Iterable<Int>>(
+            "First" to TieBreakers::createChooseFirst,
             "Max Degree" to TieBreakers::createChooseMaxDegree,
             "Min Degree" to TieBreakers::createChooseMinDegree
     )
     val secondTieBreakers = mapOf<String, (Graph<Int>) -> (Collection<Int>) -> Int>(
+            "First" to TieBreakers2::createChooseFirst,
             "Max Neighbours Degree" to TieBreakers2::createChooseMaxNeighboursDegree,
             "Min Neighbours Degree" to TieBreakers2::createChooseMinNeighboursDegree,
             "Max Edges between Neighbours" to TieBreakers2::createChooseMaxNeighboursEdges,
             "Min Edges between Neighbours" to TieBreakers2::createChooseMinNeighboursEdges
     )
 
+    println("'Graph','Vertices','Edges','Edge Density','Iterations','First Tie Breaker','Second Tie Breaker','MIM'")
     for ((graphName, graph) in graphs) {
+        val numberOfVertices = graph.nodes().size
+        val numberOfEdges = graph.edges().size
+        val edgeDensity = numberOfEdges.toDouble() / (numberOfVertices * (numberOfVertices - 1))
         for (iteration in iterations) {
             for ((firstTieBreakerName, firstTieBreaker) in firstTieBreakers) {
                 for ((secondTieBreakerName, secondTieBreaker) in secondTieBreakers) {
                     val treeDecomposition = TreeDecompositor(graph, firstTieBreaker, secondTieBreaker, iteration).compute()
-                    println("'$graphName','$iteration','$firstTieBreakerName','$secondTieBreakerName','${treeDecomposition.mimValue}'")
+                    println("'$graphName','$numberOfVertices','$numberOfEdges','$edgeDensity','$iteration','$firstTieBreakerName','$secondTieBreakerName','${treeDecomposition.mimValue}'")
                 }
             }
         }

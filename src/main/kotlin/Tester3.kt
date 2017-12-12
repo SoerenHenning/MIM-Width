6f08279ff4709a25918e1ac89d30cc3835544f81
Sören Henning <SoerenHenning@users.noreply.github.com>
2017-12-12 08:38:24 +0100

    remaining set of graphs

--- src/main/kotlin/Tester3.kt
+++ src/main/kotlin/Tester3.kt
@@ -46,7 +46,7 @@ fun main(args: Array<String>) {
     val dimacsGraphs = dimacsGraphFiles.asSequence().map{ Pair(it, DimacsImporter.importGraph(getClasspathFileReader("$directory/$it"))) }
     val corruptedDimacsGraphs = corruptedDimacsGraphFiles.asSequence().map{ Pair(it, DimacsImporter.importGraph(getClasspathFileReader("$directory/$it"),nodeIds = true)) }
     //val graphs = dimacsGraphs + corruptedDimacsGraph
-    val graphs = ExampleGraphs.graphs.filter { it.graph.nodes().size in 51..250 }
+    val graphs = ExampleGraphs.graphs.filter { it.graph.nodes().size in 251..500 }
 
     val iterations = 10
     val firstTieBreaker: (Graph<Int>, Collection<Int>) -> Iterable<Int> = ReducingTieBreakers::chooseMinDegree
